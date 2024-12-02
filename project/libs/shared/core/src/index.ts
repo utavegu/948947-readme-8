0933555c39371420225b6d9cc31e2197d52ce5ac
Utavegu <utavegu@list.ru>
2024-12-02 21:30:57 +0500

    feat: add entity factory and constants in core library

--- project/libs/shared/core/src/index.ts
+++ project/libs/shared/core/src/index.ts
@@ -2,3 +2,13 @@ export { BaseEntity } from './lib/base/entity';
 
 export { UserRoleEnum } from './lib/types/user-role.enum';
 export { IUser, IAuthUser } from './lib/types/user.interface';
+
+export { IStorableEntity } from './lib/types/storable-entity.interface';
+export { IEntityFactory } from './lib/types/entity-factory.interface';
+
+export {
+  SALT_ROUNDS,
+  AUTH_USER_EXISTS,
+  AUTH_USER_NOT_FOUND,
+  AUTH_USER_PASSWORD_WRONG,
+} from './lib/constants'
---
+++ project/libs/shared/core/src/lib/constants.ts
@@ -0,0 +1,12 @@
+const
+  SALT_ROUNDS = 10,
+  AUTH_USER_EXISTS = 'User with this email exists',
+  AUTH_USER_NOT_FOUND = 'User not found',
+  AUTH_USER_PASSWORD_WRONG = 'User password is wrong';
+
+export {
+  SALT_ROUNDS,
+  AUTH_USER_EXISTS,
+  AUTH_USER_NOT_FOUND,
+  AUTH_USER_PASSWORD_WRONG,
+}
