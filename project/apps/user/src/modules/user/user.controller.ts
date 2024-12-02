903b6033afe920f2527fb3ee7602f6cef6644578
Utavegu <utavegu@list.ru>
2024-12-02 21:34:52 +0500

    feat: base works in user module

--- project/apps/user/src/modules/user/user.controller.ts
+++ project/apps/user/src/modules/user/user.controller.ts
@@ -1,12 +1,27 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Delete, Get, Post, Put } from '@nestjs/common';
 import { UserService } from './user.service';
 
 @Controller('user')
 export class UserController {
-  constructor(private readonly userService: UserService) {}
+  constructor(private readonly userService: UserService) { }
 
   @Get()
   getData() {
     return this.userService.getData();
   }
+
+  @Post()
+  createUser() { }
+
+  @Get()
+  getAllUsers() { }
+
+  @Get()
+  getUserById() { }
+
+  @Put()
+  editUser() { }
+
+  @Delete()
+  deleteUser() { }
 }
