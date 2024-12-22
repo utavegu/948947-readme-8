07cc823d11765a7db8e21843fc85ae16781511ec
Utavegu <utavegu@list.ru>
2024-12-22 13:37:27 +0500

    feat: add prisma client module

--- project/libs/blog/models/src/index.ts
+++ project/libs/blog/models/src/index.ts
@@ -1,1 +1,2 @@
-export * from './lib/models';
+export { PrismaClientModule } from './prisma-client-module/prisma-client.module';
+export { PrismaClientService } from './prisma-client-module/prisma-client.service';
